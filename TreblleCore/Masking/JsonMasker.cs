42d8016490f6a7bd76be69dafe684fad65177dfc
wave95 <nikolaagajic1995@gmail.com>
2025-04-11 13:30:15 +0200

    remove code commented out

--- TreblleCore/Masking/JsonMasker.cs
+++ TreblleCore/Masking/JsonMasker.cs
@@ -56,64 +56,6 @@ public static class JsonMasker
         }
     }
 
-    //private static void MaskFieldsFromJToken(JToken? token, Dictionary<string, string> maskingMap, List<string> path, IServiceProvider serviceProvider, ILogger logger)
-    //{
-    //    if (token is not JContainer container)
-    //    {
-    //        return;
-    //    }
-
-    //    foreach (var jToken in container.Children())
-    //    {
-    //        if (jToken is JProperty prop)
-    //        {
-    //            var currentPath = string.Join(".", path.Concat(new[] { prop.Name }));
-
-    //            if (prop.Value is JContainer)
-    //            {
-    //                MaskFieldsFromJToken(prop.Value, maskingMap, path.Concat(new[] { prop.Name }).ToList(), serviceProvider, logger);
-    //            }
-    //            else if (prop.Value is not null)
-    //            {
-    //                bool isValueMasked = false;
-    //                foreach (KeyValuePair<string, string> map in maskingMap)
-    //                {
-
-    //                    if (shouldMap(map.Key, currentPath))
-    //                    {
-    //                        var masker = serviceProvider.GetKeyedService<IStringMasker>(map.Value);
-
-    //                        if (masker is not null)
-    //                        {
-    //                            prop.Value = masker.Mask(prop.Value.ToString());
-    //                            isValueMasked = true;
-    //                            break;
-    //                        }
-    //                        else
-    //                        {
-    //                            logger.LogError($"Could not resolve masker for field {currentPath}");
-    //                        }
-    //                    }
-    //                }
-
-    //                // if the value is not masked go over mapping once again to check if value matches any pattern
-    //                if (!isValueMasked)
-    //                {
-    //                    foreach (DefaultStringMasker masker in serviceProvider.GetServices(typeof(DefaultStringMasker)))
-    //                    {
-    //                        if (masker.IsPatternMatch(prop.Value.ToString()))
-    //                        {
-    //                            prop.Value = masker.Mask(prop.Value.ToString());
-    //                            break;
-    //                        }
-    //                    }
-    //                }
-    //            }
-    //        }
-
-    //    }
-    //}
-
     private static void maskProperty(JProperty property, string currentPath, Dictionary<string, string> maskingMap, IServiceProvider serviceProvider, ILogger logger)
     {
         bool isValueMasked = false;
