4d9778d3f1c9d4848e938339516391430b358246
Mateo Novosel <mateo.novosel2@gmail.com>
2026-02-18 14:24:40 +0100

    added response body xml conversion

--- TreblleCore/TreblleJsonContext.cs
+++ TreblleCore/TreblleJsonContext.cs
@@ -11,6 +11,7 @@ namespace Treblle.Net.Core;
 [JsonSerializable(typeof(TrebllePayload))]
 [JsonSerializable(typeof(JsonElement))]
 [JsonSerializable(typeof(object))] // For dynamic request/response bodies
+[JsonSerializable(typeof(List<object>))] 
 [JsonSerializable(typeof(string))] // For dictionary values
 [JsonSerializable(typeof(Dictionary<string, object>))] // For headers
 [JsonSerializable(typeof(Dictionary<string, string>))] // For form data and masking config
